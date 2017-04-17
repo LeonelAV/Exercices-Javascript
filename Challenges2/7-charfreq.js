aae8c06937f2f956f4536d7b3e14e9fd1e6a864a
leonel meirinho <leonel meirinho>
2017-04-17 12:09:45 +0200

    juanma resolutions

--- Challenges2/7-charfreq.js
+++ Challenges2/7-charfreq.js
@@ -5,15 +5,123 @@ Write a function charFreq() that takes a string and builds a frequency listing o
  Try it with something like charFreq("abbabcbdbabdbdbabababcbcbab").*/
 
 
- function charFreq (text) {
-   var freqs = {}
-   function eachChar (c) {
-     freqs[c] = freqs[c] + 1 || 1
+ // Way 1
+ function charFreq(text) {
+   var frequencies = {}
+   var currentChar;
+   for (var i=0; i<text.length; i++){
+     currentChar = text[i];
+     if (!frequencies[currentChar]) {
+       frequencies[currentChar] = 1
+     }
+     else {
+       frequencies[currentChar]++
+     }
    }
-   text.split('').forEach(eachChar)
-   return freqs
+   return frequencies
  }
 
- // Test
- charFreq('abbabcbdbabbdbabababcbcbab')
+ // Way 2
+ function charFreq(text) {
+   var frequencies = {}
+   var aText = text.split("")
+   aText.forEach(function(currentChar) {
+     if (!frequencies[currentChar]) {
+       frequencies[currentChar] = 1
+     }
+     else {
+       frequencies[currentChar]++
+     }
+   })
+   return frequencies
+ }
 
+ // Way 3
+ function charFreq(text) {
+   var frequencies = {}
+   var cleanedText = text.replace(/ /g, "")
+   var currentChar;
+   for (var i=0; i<cleanedText.length; i++){
+     currentChar = cleanedText[i];
+     if (!frequencies[currentChar]) {
+       frequencies[currentChar] = 1
+     }
+     else {
+       frequencies[currentChar]++
+     }
+   }
+   return frequencies
+ }
+
+ // Way 4
+ function charFreq(text) {
+   var frequencies = {}
+   var cleanedText = text.replace(/[ ,.]/g, "")
+   var currentChar;
+   for (var i=0; i<cleanedText.length; i++){
+     currentChar = cleanedText[i];
+     if (!frequencies[currentChar]) {
+       frequencies[currentChar] = 1
+     }
+     else {
+       frequencies[currentChar]++
+     }
+   }
+   return frequencies
+ }
+
+
+
+ // Way 5
+ function charFreq(text) {
+   var frequencies = {}
+   var cleanedText = text.replace(/\W/g, "")
+   var currentChar;
+   for (var i=0; i<cleanedText.length; i++){
+     currentChar = cleanedText[i];
+     if (!frequencies[currentChar]) {
+       frequencies[currentChar] = 1
+     }
+     else {
+       frequencies[currentChar]++
+     }
+   }
+   return frequencies
+ }
+
+ // Way 5
+ function charFreq(text) {
+   var frequencies = {}
+   var aText = text.match(/\w/gi) // text.match(/[a-z]/gi)
+   aText.forEach(function(currentChar) {
+     if (!frequencies[currentChar]) {
+       frequencies[currentChar] = 1
+     }
+     else {
+       frequencies[currentChar]++
+     }
+   })
+   return frequencies
+ }
+
+
+ // Way 6
+ function charFreq(text) {
+   return text
+           .match(/\w/gi)
+           .reduce( function(oFreq, currentChar) {
+             if (!oFreq[currentChar]) oFreq[currentChar] = 1
+             else oFreq[currentChar]++
+             return oFreq
+           }, {})
+ }
+
+ // Way 7
+ function charFreq(text) {
+   return text
+           .match(/\w/gi)
+           .reduce( function(oFreq, char) {
+             oFreq[char] = oFreq[char] ? ++oFreq[char] : 1 //operador ternario
+             return oFreq
+           }, {})
+ }
